df6404d430a9361adc1cbab743ccf48357feba4c
andra <andrabaktiangjaya@gmail.com>
2025-03-14 23:12:52 +0800

    Finish home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-import {NgClass, NgStyle} from '@angular/common';
+import {NgClass, NgForOf, NgIf, NgStyle} from '@angular/common';
+import {StarComponent} from '../share/star/star.component';
 
 @Component({
   selector: 'app-home',
+
   imports: [
     NgClass,
-    NgStyle
+    NgStyle,
+    NgForOf,
+    NgIf,
+    StarComponent
   ],
   templateUrl: './home.component.html',
   standalone: true,
@@ -13,24 +18,36 @@ import {NgClass, NgStyle} from '@angular/common';
 })
 export class HomeComponent {
   slides = [
-    { text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum eleifend felis. In hac habitasse platea\n" +
+    {
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum eleifend felis. In hac habitasse platea\n" +
         "      dictumst. In hac habitasse platea dictumst. Sed molestie malesuada orci non consectetur. Proin ac quam\n" +
         "      consectetur, tempor metus id, sollicitudin leo. Nulla tellus ligula, mollis id eros a, consectetur eleifend dui.\n" +
         "      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam sollicitudin\n" +
         "      ante sit amet dolor dictum ullamcorper. Nullam sed bibendum enim. Maecenas luctus sem at neque faucibus\n" +
-        "      pellentesque. Morbi purus nunc, pretium id placerat vitae, ultricies rutrum ligula. Aliquam rutrum commodo erat.", image: "image2.jpg", year: "1978–1980" },
-    { text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum eleifend felis. In hac habitasse platea\n" +
+        "      pellentesque. Morbi purus nunc, pretium id placerat vitae, ultricies rutrum ligula. Aliquam rutrum commodo erat.",
+      image: "image2.jpg",
+      year: "1978–1980"
+    },
+    {
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum eleifend felis. In hac habitasse platea\n" +
         "      dictumst. In hac habitasse platea dictumst. Sed molestie malesuada orci non consectetur. Proin ac quam\n" +
         "      consectetur, tempor metus id, sollicitudin leo. Nulla tellus ligula, mollis id eros a, consectetur eleifend dui.\n" +
         "      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam sollicitudin\n" +
         "      ante sit amet dolor dictum ullamcorper. Nullam sed bibendum enim. Maecenas luctus sem at neque faucibus\n" +
-        "      pellentesque. Morbi purus nunc, pretium id placerat vitae, ultricies rutrum ligula. Aliquam rutrum commodo erat.", image: "image3.jpg", year: "1981–1985" },
-    { text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum eleifend felis. In hac habitasse platea\n" +
+        "      pellentesque. Morbi purus nunc, pretium id placerat vitae, ultricies rutrum ligula. Aliquam rutrum commodo erat.",
+      image: "image3.jpg",
+      year: "1981–1985"
+    },
+    {
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum eleifend felis. In hac habitasse platea\n" +
         "      dictumst. In hac habitasse platea dictumst. Sed molestie malesuada orci non consectetur. Proin ac quam\n" +
         "      consectetur, tempor metus id, sollicitudin leo. Nulla tellus ligula, mollis id eros a, consectetur eleifend dui.\n" +
         "      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam sollicitudin\n" +
         "      ante sit amet dolor dictum ullamcorper. Nullam sed bibendum enim. Maecenas luctus sem at neque faucibus\n" +
-        "      pellentesque. Morbi purus nunc, pretium id placerat vitae, ultricies rutrum ligula. Aliquam rutrum commodo erat.", image: "image4.jpg", year: "1986–1990" }
+        "      pellentesque. Morbi purus nunc, pretium id placerat vitae, ultricies rutrum ligula. Aliquam rutrum commodo erat.",
+      image: "image4.jpg",
+      year: "1986–1990"
+    }
   ];
 
   currentIndex = 0;
@@ -46,5 +63,43 @@ export class HomeComponent {
   goToSlide(index: number) {
     this.currentIndex = index;
   }
+
+  testimonials = [
+    {
+      stars: 5,
+      title: 'Loved it!',
+      content: 'The product far exceeded my expectations. The team was amazing!',
+      author: 'John Doe'
+    },
+    {
+      stars: 4,
+      title: 'Pretty good experience',
+      content: 'Overall solid, a few issues here and there, but support was great.',
+      author: 'Jane Smith'
+    },
+    {
+      stars: 5,
+      title: 'Best purchase ever',
+      content: 'I can’t imagine my daily workflow without it now. 10/10 recommended!',
+      author: 'David Johnson'
+    }
+  ];
+
+  currentSlide = 0;
+
+  // Move to the next testimonial
+  nextTestimonial(): void {
+    this.currentSlide = (this.currentSlide + 1) % this.testimonials.length;
+  }
+
+  // Move to the previous testimonial
+  prevTestimonial(): void {
+    // We add `length` before the mod to avoid negative index issues
+    this.currentSlide =
+      (this.currentSlide + this.testimonials.length - 1) % this.testimonials.length;
+  }
+
+
 }
+
 
